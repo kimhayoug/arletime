7d3323ea6c36bc02517649d9d7077bd83560dc54
D7703_23 <d7703_23@D7703-23ui-iMac.local>
2018-04-11 15:36:19 +0900

    time

--- datepk/ViewController.swift
+++ datepk/ViewController.swift
@@ -12,16 +12,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var Datelabel: UILabel!
     @IBOutlet weak var myPicker: UIDatePicker!
     
+    @IBOutlet weak var mytime: UILabel!
+    var count = 0
+    let interval = 1.0
+    let timeSelector: Selector = #selector(ViewController.updateTime)
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
     }
 
     @IBAction func myPicker(_ sender: Any) {
         print("chang Date Picker")
+        
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss a EE"
         Datelabel.text = formatter.string(from: myPicker.date)
+    }
+    @objc func updateTime(){
+        let date = NSDate()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss a EE"
+        mytime.text = formatter.string(from: date as Date)
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
